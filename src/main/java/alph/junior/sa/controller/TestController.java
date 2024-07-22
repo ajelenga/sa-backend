6e14248fedece07c70604a798ee2b86b14b4d969
ajelenga <elengaalphonse107@gmail.com>
2024-07-22 23:38:56 +0200

    test

---
+++ src/main/java/alph/junior/sa/controller/TestController.java
@@ -0,0 +1,25 @@
+package alph.junior.sa.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "test")
+public class TestController {
+
+    @GetMapping(path = "string")
+    public String getString(){
+        return " Chaine de caractère transmise par SA";
+    }
+
+    @GetMapping()
+    public List<String> getList(){
+        return List.of("Chaine de caractère transmise 1", "Chaine de caractère transmise 2");
+    }
+
+}
+
+
+
---
+++ src/test/java/alph/junior/sa/SaBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package alph.junior.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaBackendApplicationTests {
+
+
+
+}
